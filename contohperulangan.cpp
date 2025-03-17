ade24ab8693364ad01a69ed637512902020bae04
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-03-17 15:36:15 +0700

    perulangan dengan pencacah naik

---
+++ contohperulangan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0
+    int perulanganDo = 0;
+
+    //perulangan dengan for 
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+{
+    cout << "nilai n = " << n << "selamat datang" << endl;
+}
+cout << "nilai n terakhir = " << n << endl;
+cout << endl;
